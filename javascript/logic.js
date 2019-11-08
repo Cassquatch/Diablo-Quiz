d48de5363c656f1ff9c8d67e594820ea0edaf03f
Cassidy Fortner <cassidy.fortner@gmail.com>
2019-11-08 14:24:50 -0600

    added basic page layout/images for background. pseudocode for js

---
+++ javascript/logic.js
@@ -0,0 +1,23 @@
+//set global element variables to use and manipulate in this file and questions object array
+
+
+
+
+
+
+
+
+//declare functions to write into the html file to display questions and answers
+//make each answer option a button
+//declare a generate  question function
+
+
+//declare a clearhtml function
+
+//countdown function
+
+
+
+
+//add event listeners to all buttons
+document.getElementById("start").addEventListener("click", );

5b7559ba73ed6ae2c4c715b01b3a61c83c2298ab
Cassidy Fortner <cassidy.fortner@gmail.com>
2019-11-08 14:55:02 -0600

    added/linked questions.js and declared the array of question objects

---
+++ javascript/questions.js
@@ -0,0 +1,28 @@
+let questions = [
+    {
+        question: "Who is the Lord of Lies?",
+        choices: ["Belial", "Diablo", "Malthael", "Tyrael"],
+        answer: "Belial"
+    }, 
+    {
+        question: "What is the Worldstone also known as?", 
+        choices: ["A pretty rock", "Eye of Anu", "Stone of Jordan", "Tal Rasha's Lidless Eye"], 
+        answer: "Eye of Anu"
+    }, 
+    {
+        question: "What is the war between the forces of the High Heavens and The Burning Hells called?", 
+        choices: ["The Devil's War", "Angels and Demons", "Fight for Sanctuary", "The Eternal Conflict"], 
+        answer: "The Eternal Conflict"
+    }, 
+    {
+        question: "Who was the last Horadrim?", 
+        choices: ["Deckard Cain", "Akkarat", "Zunimassa", "Trag'oul"], 
+        answer: "Deckard Cain"
+    }, 
+    {
+        question: "Who destroyed the Worldstone?", 
+        choices: ["Deckard Cain", "Andariel", "Imperius", "Tyrael"],
+        answer: "Tyrael"
+
+    }
+]
